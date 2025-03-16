5a8c11ee5f5843c53b26dd270b705d33c3defbe2
Jordan514 <94439800+Jordan514@users.noreply.github.com>
2025-03-15 21:19:30 -0500

    finish importing all prevous elements into pageload.js

--- src/pageload.js
+++ src/pageload.js
@@ -1,23 +1,41 @@
 function addElementToContent(
   element = "div",
-  textContent,
-  arrayOfAttributeObjects
+  textContent = "",
+  AttributeObjects = []
 ) {
   let content = document.querySelector("#content");
+
   let newElement = document.createElement(element);
-  newElement.textContent = textContent;
-  arrayOfAttributeObjects.forEach((obj) =>
+
+  textContent ? (newElement.textContent = textContent) : undefined;
+
+  AttributeObjects.forEach((obj) =>
     newElement.setAttribute(obj.attribute, obj.data)
   );
 
   content.appendChild(newElement);
-  return;
 }
 
 function pageload() {
   addElementToContent("div", "Jordan's Burger Factory", [
     { attribute: "class", data: "headline" },
   ]);
+  addElementToContent("img", "", [
+    {
+      attribute: "src",
+      data: "https://www.thecookierookie.com/wp-content/uploads/2023/04/featured-stovetop-burgers-recipe.jpg",
+    },
+    { attribute: "alt", data: "" },
+    { attribute: "style", data: "height: 500px; width: auto" },
+  ]);
+  addElementToContent(
+    "div",
+    `Jordans Burger factory is full of the best burgers in america! From our
+        classic american cheeseburger to our portobella new jersey burger to our
+        california vegan burger we have the best burgers in united States of
+        burgers!`,
+    [{ attribute: "class", data: "description" }]
+  );
 }
 
 export { pageload };
